76e31e6f4fd953890cb7a39627892bfc849909de
kcichy37 <kcichy37@gmail.com>
2022-06-04 17:04:16 +0000

    Add function gameChange to js which changes rules section display to none and game board to block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
-function gameChange() {
-    let 
+function gameChange(){
+    let rulesChange = document.getElementById('game--rules');
+        if (rulesChange.style.display === 'none') {
+            rulesChange.style.display = 'block';
+        } else {
+            rulesChange.style.display = 'none';
+        };
+
+    let gameBoard = document.getElementById('game--board');
+        if(rulesChange.style.display === 'none'){ 
+            gameBoard.style.display = 'block';
+            console.log('yes');
+        }
 }
