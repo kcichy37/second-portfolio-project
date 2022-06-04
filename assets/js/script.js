8a33a69ab9fca515353068055988e440c4bfcb4b
kcichy37 <kcichy37@gmail.com>
2022-06-04 20:31:16 +0000

    Add javaScript functionality to make X and O appear on game board after user clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,27 @@ function gameChange(){
             rulesChange.style.display = 'none';
         };
 
-    const gameBoard = document.getElementById('game--board');
+    const gameBoardSection = document.getElementById('game--board');
         if(rulesChange.style.display === 'none'){ 
-            gameBoard.style.display = 'block';
+            gameBoardSection.style.display = 'block';
         }
 }
+
+
+
+let cells = Array.from(document.getElementsByClassName('cell'));
+const O = 'O';
+const X = 'X';
+let currentPlayer = X
+
+const startGame = () => {
+    cells.forEach(cell => cell.addEventListener('click', cellClicked, { once: true}));
+}
+
+function cellClicked(e){
+    e.target.innerText = currentPlayer;
+
+    currentPlayer = currentPlayer == X ? O : X; 
+}
+
+startGame()
