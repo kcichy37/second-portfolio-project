40f98ca5cc60793f55fada02b94afec3b856cdd8
kcichy37 <kcichy37@gmail.com>
2022-06-08 19:47:58 +0000

    Minor adjustments and initial commit for Doc folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,14 @@
  * result is Game board section display changed to 'block' */
 function gameChange() {
     const rulesChange = document.getElementById('game--rules');
+
     //Hides Game rules
     if (rulesChange.style.display === 'none') {
         rulesChange.style.display = 'block';
     } else {
         rulesChange.style.display = 'none';
-    };
+    }
+
     //Shows game board after game rules is hidden
     const gameBoardSection = document.getElementById('game--board');
     if (rulesChange.style.display === 'none') {
@@ -23,35 +25,35 @@ const O = 'O';
 const X = 'X';
 let currentPlayer = X;
 let cellsIndex = Array(9).fill(null);
-let movesLeft = 9
+let movesLeft = 9;
 
 //Start of game functions
 const startGame = () => {
     cells.forEach(cell => cell.addEventListener('click', cellClicked, {
         once: true
     }));
-}
+};
 
 //function to allow users to put their marks down
 function cellClicked(e) {
-    const id = e.target.id
+    const id = e.target.id;
 
     //targets ID's of the cells to then place the current players mark
     if (!cellsIndex[id]) {
-        cellsIndex[id] = currentPlayer
+        cellsIndex[id] = currentPlayer;
         e.target.innerText = currentPlayer;
-        movesLeft--
+        movesLeft--;
 
         //function to declare a winner
         if (playerWon() !== false) {
 
             //Disabled users to place marks after winner is declared
-            cells.forEach(cell => cell.removeEventListener('click', cellClicked))
+            cells.forEach(cell => cell.removeEventListener('click', cellClicked));
 
             //changes winners marks colour to blue to show where the win occured
-            let winnercolor = getComputedStyle(document.body).getPropertyValue('--winningcolor')
+            let winnercolor = getComputedStyle(document.body).getPropertyValue('--winningcolor');
             let winning = playerWon();
-            winning.map(cell => cells[cell].style.color = winnercolor)
+            winning.map(cell => cells[cell].style.color = winnercolor);
 
             /**Once user is declared a winner
              * this will change the game board display to 'none'
@@ -67,13 +69,13 @@ function cellClicked(e) {
             }, 2500);
 
             //Changes sentence to show who won between X or O
-            document.getElementById('whowon').innerText = `Player ${currentPlayer} Has Won!`
+            document.getElementById('whowon').innerText = `Player ${currentPlayer} Has Won!`;
 
             //Adds score to the winner 
             if (currentPlayer == X) {
-                xMarkScore()
+                xMarkScore();
             } else {
-                oMarkScore()
+                oMarkScore();
             }
 
             /**Declares a Tie 
@@ -86,7 +88,7 @@ function cellClicked(e) {
                 document.getElementById('game--board').style.display = 'none';
                 document.getElementById('game--score').style.display = "block";
             }, 1000);
-            document.getElementById('whowon').innerText = `It's A Tie`
+            document.getElementById('whowon').innerText = `It's A Tie`;
         }
 
         //Switches turns between users. 
@@ -97,11 +99,11 @@ function cellClicked(e) {
         let right = document.getElementById('right');
 
         if (currentPlayer == O) {
-            right.style.color = '#ffffff'
-            left.style.color = '#fcd03e'
+            right.style.color = '#ffffff';
+            left.style.color = '#fcd03e';
         } else if (currentPlayer == X) {
-            left.style.color = '#ffffff'
-            right.style.color = '#fcd03e'
+            left.style.color = '#ffffff';
+            right.style.color = '#fcd03e';
         }
     }
 
@@ -121,7 +123,7 @@ const winningConditions = [
     [2, 5, 8],
     [2, 4, 6],
     [0, 4, 8]
-]
+];
 
 //Function to determine which combination marks need to be in to be a winner
 function playerWon() {
@@ -153,30 +155,30 @@ function gameRestart() {
     const gameBoardSection = document.getElementById('game--board');
     const gameScore = document.getElementById('game--score');
     movesLeft = 9;
-    clearBoard()
+    clearBoard();
 
     function clearBoard() {
         //Resets cells innerHTML to nothing
-        document.getElementById("0").innerHTML = ''
-        document.getElementById("1").innerHTML = ''
-        document.getElementById("2").innerHTML = ''
-        document.getElementById("3").innerHTML = ''
-        document.getElementById("4").innerHTML = ''
-        document.getElementById("5").innerHTML = ''
-        document.getElementById("6").innerHTML = ''
-        document.getElementById("7").innerHTML = ''
-        document.getElementById("8").innerHTML = ''
+        document.getElementById("0").innerHTML = '';
+        document.getElementById("1").innerHTML = '';
+        document.getElementById("2").innerHTML = '';
+        document.getElementById("3").innerHTML = '';
+        document.getElementById("4").innerHTML = '';
+        document.getElementById("5").innerHTML = '';
+        document.getElementById("6").innerHTML = '';
+        document.getElementById("7").innerHTML = '';
+        document.getElementById("8").innerHTML = '';
 
         //Resets cells color after winners changed color 
-        document.getElementById("0").style.color = '#fcd03e'
-        document.getElementById("1").style.color = '#fcd03e'
-        document.getElementById("2").style.color = '#fcd03e'
-        document.getElementById("3").style.color = '#fcd03e'
-        document.getElementById("4").style.color = '#fcd03e'
-        document.getElementById("5").style.color = '#fcd03e'
-        document.getElementById("6").style.color = '#fcd03e'
-        document.getElementById("7").style.color = '#fcd03e'
-        document.getElementById("8").style.color = '#fcd03e'
+        document.getElementById("0").style.color = '#fcd03e';
+        document.getElementById("1").style.color = '#fcd03e';
+        document.getElementById("2").style.color = '#fcd03e';
+        document.getElementById("3").style.color = '#fcd03e';
+        document.getElementById("4").style.color = '#fcd03e';
+        document.getElementById("5").style.color = '#fcd03e';
+        document.getElementById("6").style.color = '#fcd03e';
+        document.getElementById("7").style.color = '#fcd03e';
+        document.getElementById("8").style.color = '#fcd03e';
     }
 
     //Changes cells data to null again and allows users to input new X or O
