86ed982d3fd1796a36fc3b4ef187717a5d751a2c
kcichy37 <kcichy37@gmail.com>
2022-06-08 18:35:27 +0000

    Add comments to JavaScript code and format document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,19 @@
-/** This function allows there to be different sections at different times.
- * First section is the "Game Rules", we can proceed futher by clicking the play button
- * this will hide the rules and show the "Game Board" section.
- * Next section which is the "Game Score" will show only once the game is completed,
- * and will be triggered by a Win, Loss, or Tie.
- */
-
-/* Game rules to Game board*/
-function gameChange(){
+/**Changes Game rules section display to 'none'
+ * activated by 'Play!' button
+ * result is Game board section display changed to 'block' */
+function gameChange() {
     const rulesChange = document.getElementById('game--rules');
-        if (rulesChange.style.display === 'none') {
-            rulesChange.style.display = 'block';
-        } else {
-            rulesChange.style.display = 'none';
-        };
-
+    //Hides Game rules
+    if (rulesChange.style.display === 'none') {
+        rulesChange.style.display = 'block';
+    } else {
+        rulesChange.style.display = 'none';
+    };
+    //Shows game board after game rules is hidden
     const gameBoardSection = document.getElementById('game--board');
-        if(rulesChange.style.display === 'none'){ 
-            gameBoardSection.style.display = 'block';
-        }
+    if (rulesChange.style.display === 'none') {
+        gameBoardSection.style.display = 'block';
+    }
 }
 
 
@@ -29,62 +25,93 @@ let currentPlayer = X;
 let cellsIndex = Array(9).fill(null);
 let movesLeft = 9
 
-
-let winnercolor = getComputedStyle(document.body).getPropertyValue('--winningcolor')
-let resetcolor = getComputedStyle(document.body).getPropertyValue('--resetcolor')
-
+//Start of game functions
 const startGame = () => {
-    cells.forEach(cell => cell.addEventListener('click', cellClicked, { once: true}));
+    cells.forEach(cell => cell.addEventListener('click', cellClicked, {
+        once: true
+    }));
 }
 
-function cellClicked(e){
+//function to allow users to put their marks down
+function cellClicked(e) {
     const id = e.target.id
 
-    if(!cellsIndex[id]){
+    //targets ID's of the cells to then place the current players mark
+    if (!cellsIndex[id]) {
         cellsIndex[id] = currentPlayer
         e.target.innerText = currentPlayer;
         movesLeft--
 
-        if(playerWon() !==false){
-            let winning = playerWon();
+        //function to declare a winner
+        if (playerWon() !== false) {
+
+            //Disabled users to place marks after winner is declared
             cells.forEach(cell => cell.removeEventListener('click', cellClicked))
 
-            winning.map(cell => cells[cell].style.color=winnercolor)
+            //changes winners marks colour to blue to show where the win occured
+            let winnercolor = getComputedStyle(document.body).getPropertyValue('--winningcolor')
+            let winning = playerWon();
+            winning.map(cell => cells[cell].style.color = winnercolor)
 
-            setTimeout(() => {document.getElementById('game--board').style.display = 'none';
-            document.getElementById('game--score').style.display = "block"; }, 2500);
+            /**Once user is declared a winner
+             * this will change the game board display to 'none'
+             * and switch to game score by changing game score
+             * display to 'block'.
+             * 
+             * Delay is set in place before the change in section
+             * for user to be able to easily identify where the win occured
+             */
+            setTimeout(() => {
+                document.getElementById('game--board').style.display = 'none';
+                document.getElementById('game--score').style.display = "block";
+            }, 2500);
 
+            //Changes sentence to show who won between X or O
             document.getElementById('whowon').innerText = `Player ${currentPlayer} Has Won!`
 
-            if(currentPlayer == X){
+            //Adds score to the winner 
+            if (currentPlayer == X) {
                 xMarkScore()
-            }else{
+            } else {
                 oMarkScore()
             }
-        }else if(movesLeft == 0){
-            setTimeout(() => {document.getElementById('game--board').style.display = 'none';
-            document.getElementById('game--score').style.display = "block"; }, 2500);
+
+            /**Declares a Tie 
+             * Tie is only shown once all 9 cells are taken
+             * such is counted by users moves 'movesLeft'
+             * once it hits 0 it's a tie
+             */
+        } else if (movesLeft == 0) {
+            setTimeout(() => {
+                document.getElementById('game--board').style.display = 'none';
+                document.getElementById('game--score').style.display = "block";
+            }, 1000);
             document.getElementById('whowon').innerText = `It's A Tie`
         }
-          
+
+        //Switches turns between users. 
+        currentPlayer = currentPlayer == X ? O : X;
+
+        //Displays which users turn it is by changing X or O colour
         let left = document.getElementById('left');
         let right = document.getElementById('right');
-        currentPlayer = currentPlayer == X ? O : X;
-        
-        if(currentPlayer == O){
+
+        if (currentPlayer == O) {
             right.style.color = '#ffffff'
             left.style.color = '#fcd03e'
-        } else if (currentPlayer == X){
+        } else if (currentPlayer == X) {
             left.style.color = '#ffffff'
             right.style.color = '#fcd03e'
         }
     }
 
-    const pop = document.getElementById("audio"); 
-    pop.play(); 
+    //Audio for when cells are clicked 
+    const pop = document.getElementById("audio");
+    pop.play();
 
 }
 
+//Winning combinations
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -96,35 +123,40 @@ const winningConditions = [
     [0, 4, 8]
 ]
 
-function playerWon(){
-    for(const condition of winningConditions){
+//Function to determine which combination marks need to be in to be a winner
+function playerWon() {
+    for (const condition of winningConditions) {
         let [a, b, c] = condition;
 
-        if(cellsIndex[a] && (cellsIndex[a] == cellsIndex[b] && cellsIndex[a] == cellsIndex[c])) {
-        return [a, b, c];
-        } 
-    }  
+        //Checks for marks position, if 3 same marks next to each other its a win
+        if (cellsIndex[a] && (cellsIndex[a] == cellsIndex[b] && cellsIndex[a] == cellsIndex[c])) {
+            return [a, b, c];
+        }
+    }
     return false;
 }
 
-
-function xMarkScore(){
+//Increments X score if win
+function xMarkScore() {
     let xscore = parseInt(document.getElementById("x--score").innerText);
     document.getElementById("x--score").innerText = ++xscore;
 }
 
-function oMarkScore(){
-    let oscore =parseInt(document.getElementById("o--score").innerText);
+//Increments O score if win
+function oMarkScore() {
+    let oscore = parseInt(document.getElementById("o--score").innerText);
     document.getElementById("o--score").innerText = ++oscore;
 }
 
-function gameRestart(){
+//Game restart 
+function gameRestart() {
     const gameBoardSection = document.getElementById('game--board');
-    const gameScore = document.getElementById('game--score'); 
+    const gameScore = document.getElementById('game--score');
     movesLeft = 9;
     clearBoard()
 
     function clearBoard() {
+        //Resets cells innerHTML to nothing
         document.getElementById("0").innerHTML = ''
         document.getElementById("1").innerHTML = ''
         document.getElementById("2").innerHTML = ''
@@ -135,6 +167,7 @@ function gameRestart(){
         document.getElementById("7").innerHTML = ''
         document.getElementById("8").innerHTML = ''
 
+        //Resets cells color after winners changed color 
         document.getElementById("0").style.color = '#fcd03e'
         document.getElementById("1").style.color = '#fcd03e'
         document.getElementById("2").style.color = '#fcd03e'
@@ -146,26 +179,28 @@ function gameRestart(){
         document.getElementById("8").style.color = '#fcd03e'
     }
 
-        cellsIndex = Array(9).fill(null);
-        cells.forEach(cell => cell.addEventListener('click', cellClicked, {once: true}));
+    //Changes cells data to null again and allows users to input new X or O
+    cellsIndex = Array(9).fill(null);
+    cells.forEach(cell => cell.addEventListener('click', cellClicked, {
+        once: true
+    }));
 
-    if( gameBoardSection.style.display === 'none'){
+    //Changes from score section to game board again
+    if (gameBoardSection.style.display === 'none') {
         gameBoardSection.style.display = 'block';
         gameScore.style.display = 'none';
     }
-
 }
 
-function quitButton(){
+//Function to quit game and takes to Thank you section
+function quitButton() {
     const thankYou = document.getElementById('thankyou');
-    const gameScore = document.getElementById('game--score'); 
+    const gameScore = document.getElementById('game--score');
 
-
-    if(gameScore.style.display === 'block'){
+    if (gameScore.style.display === 'block') {
         gameScore.style.display = 'none';
         thankYou.style.display = 'block';
     }
 }
 
 startGame();
-
